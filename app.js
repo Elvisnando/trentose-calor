7b88262397cfe07411ab592675b1300eb40ec2f1
elvisnando <elvis_work@hotmail.it>
2015-11-24 16:55:26 +0100

    calcolo min max

--- app.js
+++ app.js
@@ -2,8 +2,43 @@
 
 $(document).ready(function(){
   
+    console.log(model.maxtemp("Wednesday"));
+    console.log(model.mintemp("Wednesday"));
   
 });
+var view ={
+
+
+};
+
+var model = {
+     maxtemp: function(giorno){
+    var max=0;
+    for(var i=0;i<data.length;i++){
+    if(data[i].day== giorno)
+    if(data[i].temperature>max)
+       max=data[i].temperature;
+
+}
+return max;
+ },
+    mintemp: function(giorno){
+    
+    var min=50;
+    for(var i=0;i<data.length;i++){
+    if(data[i].day== giorno)
+    if(data[i].temperature<min)
+       min=data[i].temperature;
+    
+    }
+        return min;
+
+}
+    };
+var controll={
+
+};
+
 
 
 
